e5d373956f992802723b87945254614b8db2763d
Victor <victor@karotte>
2017-07-27 18:55:17 +0200

    [TASK]Require Symfony/Console in index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+use Symfony\Component\Console\Application;
+
+
+$application = new Application();
+
+// ... register commands
+
+$application->run();
+
+echo "Hallo Welt";
